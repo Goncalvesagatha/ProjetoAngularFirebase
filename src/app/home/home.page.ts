5f9bf63a90c6320a25ac29a251c6cc9ced7f8887
Goncalvesagatha <140774528+Goncalvesagatha@users.noreply.github.com>
2024-08-31 01:46:26 +0000

    paises

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,10 @@
+
 import { Component } from '@angular/core';
 import { AuthenticateService } from '../services/auth.service';
 import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,226 +12,35 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+ paises: any;
+ isLoading: boolean = false;
+ 
+  constructor(
+    private router: Router
+  ){
+this.getPaisCode();
+ }
 
-  perfil = {
-    nome: 'Agatha Cristie Gonçalves',
-    foto: 'https://media.licdn.com/dms/image/v2/C4D03AQHd3RNtjU8iRQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1561488372851?e=2147483647&v=beta&t=77b3mwMXc1ppGisXLE3rCKFYzNGBfqwdBuRsY5EOLJM',
-    profissao: 'Analista de Dados Sr',
-    instagram: '@goncalvesagatha',
-    idioma: 'Português/Inglês',
-    cidade: 'Tatuí-SP',
-    inicio: 'Desde Janeiro/2017'
-  };
-  biografia = {
-    texto: 'BodyBuilder nas horas vagas'
+ getPaisCode(){
+  this.isLoading = true;
+  fetch('https://restcountries.com/v3.1/all?fields=name,ccn3,flags')
+  .then(dados => dados.json())
+  .then(dados =>{
+    console.log(dados);
+    this.paises=dados;
+  })
+  .catch(erro =>{
+    console.log(erro);
+  })
+  .finally(() =>{
+    this.isLoading = false;
+  })
+ }
 
-  };
+ verDetalhes(ccn3: any){
+  this.router.navigate(['detalhe-pais'],{
+    state: { codigo: ccn3 }
+  });
 
-  metrica = {
-    curtidas: '1,5k',
-    seguindo: '0',
-    seguidores: '2MM'
-  }
-
-  postagens = [
-    {
-      perfil: {
-        nome: 'Juliana',
-        mencao_perfil: '@juliana',
-        foto: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Novas fotos de gatos',
-        foto: 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Antonieta',
-        mencao_perfil: '@antonieta',
-        foto: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Veja nova lamborghini',
-        foto: 'https://images.pexels.com/photos/2127733/pexels-photo-2127733.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Silveiro',
-        mencao_perfil: '@silveiro',
-        foto: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Olá pessoal, alguém quer aprender CSS?',
-        foto: null
-      }
-    },
-    {
-      perfil: {
-        nome: 'Marcos',
-        mencao_perfil: '@marcos',
-        foto: 'https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Se colocar uma calculadora na geladeira, terei tudo friamente calculado?',
-        foto: null
-      }
-    },
-    {
-      perfil: {
-        nome: 'Marcos',
-        mencao_perfil: '@marcos',
-        foto: 'https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Se colocar uma calculadora na geladeira, terei tudo friamente calculado?',
-        foto: null
-      }
-    },
-    {
-      perfil: {
-        nome: 'Mariana',
-        mencao_perfil: '@mariana',
-        foto: 'https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Se um carro com adesivo guiado por Deus bater, de quem é a culpa?',
-        foto: 'https://images.pexels.com/photos/3660425/pexels-photo-3660425.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Mônica',
-        mencao_perfil: '@mmmica',
-        foto: 'https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Galera! Se dois irmãos gêmeos se casam com duas irmãs gêmeas, os filhos vão ser iguais???',
-        foto: null
-      }
-    },
-    {
-      perfil: {
-        nome: 'Koringa',
-        mencao_perfil: '@kor',
-        foto: 'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Como fala o problema é seu na língua da sua região??',
-        foto: 'https://images.pexels.com/photos/50859/pexels-photo-50859.jpeg?auto=compress&cs=tinysrgb&w=800'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Juliana',
-        mencao_perfil: '@juliana',
-        foto: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Alguém sabe que dinossauro é esse?',
-        foto: 'https://images.pexels.com/photos/161959/prehistoric-times-urtier-museum-exhibit-161959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Juliana',
-        mencao_perfil: '@juliana',
-        foto: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Alguém sabe o nome dessa comida?',
-        foto: 'https://images.pexels.com/photos/410857/pexels-photo-410857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Julia',
-        mencao_perfil: '@julia',
-        foto: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-      },
-      postagem: {
-        mensagem: 'O que você faria se soubesse que tem apenas mais 24 horas de vida?',
-        foto: null
-      }
-    },
-    {
-      perfil: {
-        nome: 'Pedro',
-        mencao_perfil: '@pedro',
-        foto: 'https://images.pexels.com/photos/936722/pexels-photo-936722.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Não confie em tudo que lê na internet',
-        foto: 'https://images.pexels.com/photos/47453/pexels-photo-47453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Maria',
-        mencao_perfil: '@maria',
-        foto: 'https://images.pexels.com/photos/834863/pexels-photo-834863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Hoje é um bom dia para ser feliz',
-        foto: null
-      }
-    },
-    {
-      perfil: {
-        nome: 'Ana',
-        mencao_perfil: '@ana',
-        foto: 'https://images.pexels.com/photos/3645474/pexels-photo-3645474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-      postagem: {
-        mensagem: 'Por que não existem elevadores no Pólo Norte?',
-        foto: 'https://images.pexels.com/photos/6212265/pexels-photo-6212265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      }
-    },
-    {
-      perfil: {
-        nome: 'Gabriel',
-        mencao_perfil: '@gabriel',
-        foto: 'https://images.pexels.com/photos/7585838/pexels-photo-7585838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-      },
-        postagem: {
-          mensagem: 'Qual é a melhor forma de se preparar para uma entrevista de emprego?',
-          foto: null
-        }
-      },
-      {
-        perfil: {
-          nome: 'Laura',
-          mencao_perfil: '@laura',
-          foto: 'https://images.pexels.com/photos/6695787/pexels-photo-6695787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-        },
-        postagem: {
-          mensagem: 'Você já ouviu falar da técnica Pomodoro?',
-          foto: 'https://images.pexels.com/photos/1181377/pexels-photo-1181377.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-        }
-      },
-      {
-        perfil: {
-          nome: 'Felipe',
-          mencao_perfil: '@felipe',
-          foto: 'https://images.pexels.com/photos/3991820/pexels-photo-3991820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-        },
-        postagem: {
-          mensagem: 'O que é mais importante na vida?',
-          foto: null
-        }
-      },
-      {
-        perfil: {
-          nome: 'Julia',
-          mencao_perfil: '@julia',
-          foto: 'https://images.pexels.com/photos/4722061/pexels-photo-4722061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-        },
-        postagem: {
-          mensagem: 'Como manter uma rotina de estudos eficiente?',
-          foto: 'https://images.pexels.com/photos/5792692/pexels-photo-5792692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-        }
-      }    
-  ]
-  constructor(){ }
+ }
 }
